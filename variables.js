fbd8ad8b003d21bbef8b86af28d84b8f7cc4d9ae
Dylan Roets <dylanroets@yahoo.com>
2022-09-29 13:23:52 -0500

    first commit

---
+++ variables.js
@@ -0,0 +1,16 @@
+console.log ( 'js' );
+
+// declare and initialize a variable
+let testVar = 9;
+
+console.log( testVar );
+
+console.log( 'the value of testVar is', testVar );
+
+testVar = 546737;
+
+console.log( 'the value of testVar is', testVar );
+
+testVar = 'just some text';
+
+console.log( 'the value of testVar is', testVar );
